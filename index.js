aa6c89954be9d5f6ff64a2d35b0f45a803f16a24
Marwan Sa-ad <saadmarwan16@gmail.com>
2021-01-14 21:44:21 +0300

    Designed services

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const navToggle = document.querySelector('.nav-toggle');
+    const navLinks = document.querySelectorAll('.nav__link');
+
+    navToggle.addEventListener('click', () => {
+        document.body.classList.toggle('nav-open');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            document.body.classList.remove('nav-open');
+        })
+    });
+})
